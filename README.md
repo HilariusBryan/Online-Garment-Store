f8dee6245392731bd1c77321e24a18e4a55227de
HilariusBryan <bumblebee.15hbr@gmail.com>
2020-07-25 12:02:38 +0700

    Final commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# Online Garment Store
- Online garment store with front end using vue js
+# garment
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+   <HeaderGarment />
+   <HeroGarment />
+   <WomenGarment />
+   <InstaGarment />
+   <PartnerGarment />
+   <FooterGarment />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderGarment from '@/components/HeaderGarment.vue';
+import HeroGarment from '@/components/HeroGarment.vue';
+import WomenGarment from '@/components/WomenGarment.vue';
+import InstaGarment from '@/components/InstaGarment.vue';
+import PartnerGarment from '@/components/PartnerGarment.vue';
+import FooterGarment from '@/components/FooterGarment.vue';
+
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderGarment,
+    HeroGarment,
+    WomenGarment,
+    InstaGarment,
+    PartnerGarment,
+    FooterGarment
+    
+  }
+}
+</script>
